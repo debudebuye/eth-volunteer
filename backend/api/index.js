21b79c1dc0d5360a0c81e8a146d16a0cd92deec8
debudebuye <debudebuye@gmail.com>
2025-03-03 00:37:46 -0800

    updates on api gate way

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path"); // Import the path module
-const connectDB = require("./config/db");
+const connectDB = require("../config/db");
 
 const app = express();
 connectDB();
@@ -10,19 +10,19 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-const eventsRoute = require("./routes/eventRoutes"); // Adjust the path if needed
-const adminRoute = require("./routes/adminRoutes");
+const eventsRoute = require("../routes/eventRoutes"); // Adjust the path if needed
+const adminRoute = require("../routes/adminRoutes");
 
 app.use("/api/events", eventsRoute); // Mount the event routes under '/events'
 app.use("/api/admin", adminRoute);
 
-const authRoutes = require("./routes/auth");
+const authRoutes = require("../routes/auth");
 app.use("/api/auth", authRoutes); // Handle API routes
 
-const ngoRoutes = require("./routes/ngoRoutes");
+const ngoRoutes = require("../routes/ngoRoutes");
 app.use("/api/ngo", ngoRoutes);
 
-const userRoutes = require("./routes/usersRoute");
+const userRoutes = require("../routes/usersRoute");
 app.use("/api", userRoutes);
 
 // Serve static files from the "uploads" directory
@@ -32,5 +32,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
+
+app.get("/",(req , res )=> res.status(200).json({ message:'server running '}))
 
 app.listen(5000, () => console.log("Server running on port 5000"));
